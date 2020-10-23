abf89a73e5cc8a01d673d1b37f17e0a9020aaadd
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-10-22 22:12:05 -0500

    Fix happy little accident with raise layer

--- keyboards/crkbd/rev1/common/keymaps/quantum/keymap.c
+++ keyboards/crkbd/rev1/common/keymaps/quantum/keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_LCTL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_F6,
                 KC_LSFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
 
-                KC_LGUI, KC_TRNS, KC_SPC, KC_ENT, MO(3), KC_RALT
+                KC_LGUI, MO(_ADJUST), KC_SPC, KC_ENT, KC_TRNS, KC_RALT
         ),
 	[_RAISE] = LAYOUT_split_3x6_3(
                 KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
                 KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,             KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV, KC_LSFT,
                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
 
-                KC_LGUI, MO(3), KC_SPC, KC_ENT, KC_TRNS, KC_RALT
+                KC_LGUI, KC_TRNS, KC_SPC, KC_ENT, MO(_ADJUST), KC_RALT
         ),
 	[_ADJUST] = LAYOUT_split_3x6_3(
                 RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
