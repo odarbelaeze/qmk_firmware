aaa9cc553e1dc70e08a19a0515f0834c387ed75a
Oscar Arbelaez <oscar.arbelaez@nextroll.com>
2020-12-03 13:52:07 -0500

    Enable more keys

--- keyboards/crkbd/rev1/common/keymaps/quantum/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/quantum/rules.mk
@@ -1,7 +1,8 @@
 SPLIT_KEYBOARD = yes
 OLED_DRIVER_ENABLE = yes
 LOC_OPTIMIZATION = yes
-NKRO_ENABLE=yes
+NKRO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
