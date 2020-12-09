f9bf9be913a0a6830a0585d0db81aa830cc204f9
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-12-09 11:57:03 -0500

    Try making an insert modifier

--- keyboards/crkbd/rev1/common/keymaps/quantum/keymap.c
+++ keyboards/crkbd/rev1/common/keymaps/quantum/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RALT_T(KC_ESC),  KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, RALT_T(KC_QUOT),
         KC_LSFT, LCTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B,          KC_N, KC_M, KC_COMM, KC_DOT, RCTL_T(KC_SLSH), KC_RSFT,
 
-        KC_LGUI, LT(_RAISE ,KC_BSPC), KC_SPC, KC_SPC, LT(_LOWER,KC_ENT), KC_LALT
+        KC_LGUI, LT(_RAISE ,KC_BSPC), KC_SPC, MT(MOD_INS, KC_SPC), LT(_LOWER,KC_ENT), KC_LALT
     ),
     [_LOWER] = LAYOUT_split_3x6_3(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                 KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
